8837dcc546f4174104017f2a011dca8f45efa1d0
mac <>
2025-06-27 15:20:41 +0530

    carco file add

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // Your custom CRACO configuration
+  // For example:
+  // webpack: {
+  //   configure: (webpackConfig) => {
+  //     return webpackConfig;
+  //   }
+  // }
+}; 
